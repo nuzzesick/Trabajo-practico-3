9e1f2f715be9cebd1f5378394b02e15b1d65038a
Nuzze <mati280341@gmail.com>
2019-11-08 14:24:11 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function windows() {
+  window.open("http://download.processing.org/processing-3.5.3-windows64.zip", "_blank");
+}
+function linux() {
+  window.open("http://download.processing.org/processing-3.5.3-linux64.tgz", "_blank");
+}
+function mac() {
+  window.open("http://download.processing.org/processing-3.5.3-macosx.zip", "_blank");
+}
